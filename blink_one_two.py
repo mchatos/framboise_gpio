f6a09f3d28f461191b63ce6e4b4f28cd9ef950f5
mchatos <37662107+mchatos@users.noreply.github.com>
2018-10-27 12:05:45 +0200

    Update blink_one_two.py

--- blink_one_two.py
+++ blink_one_two.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 """
-blink_one_by_one.py
-Leds clignotantes une par une sur GPIO via des résistances de 330 Ohms
+blink_one_two.py
+Leds clignotantes une par une par deux par une sur GPIO via des résistances de 330 Ohms
 logiciel            : python 3.4.2
 cible               : raspberry Pi
 date de création    : 20/10/2018
@@ -32,7 +32,8 @@ def button_callback(channel):
 # VAR et GPIO
 button=27
 #leds=[21,20,16,1,7,8,25,24]								#Declaration array leds
-leds=[26,16,6,5,25,24,23,22]	
+leds=[26,16,6,5,25,24,23,22]
+nbLeds=len(leds)
 
 GPIO.setwarnings(False)                 				#désactive le mode warning
 GPIO.setmode(GPIO.BCM)                  				#utilisation des numéros de ports du processeur
@@ -43,13 +44,21 @@ GPIO.add_event_detect(button,GPIO.RISING,callback=button_callback)
 #-------------------------------------------------------------------------------
 # main
 if __name__ == '__main__':
-
+	
 		print("Début du clignotements des LEDs")
 		while True :                        				#boucle infinie
 			for i in range(len(leds)) :
 				print ("led : ", leds[i],"led -1 : ", leds[i-1])
+
+				if leds[i] == leds[-nbLeds] :			#On verifie si on est sur la premiere led
+					GPIO.output(leds[nbLeds-1], GPIO.LOW)	#On eteint la derniere led
+					
+				GPIO.output(leds[i-2], GPIO.LOW)
 				GPIO.output(leds[i], GPIO.HIGH)
-				GPIO.output(leds[i-1], GPIO.LOW)
-				time.sleep(0.3)
+				time.sleep(1)
+				
+				if leds[i] == leds[nbLeds-1] :			#On verifie si on est sur la derniere led
+					GPIO.output(leds[i-1], GPIO.LOW)	#On eteint la led precedente
+					time.sleep(1)
 
 #-------------------------------------------------------------------------------
